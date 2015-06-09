c610b044cae55781f3060a77654b95ed7c1d888e
AngeLopez <angel@angelopez.ca>
2015-06-09 14:28:27 -0400

    Inputs added and year born calculated

--- exercise3.rb
+++ exercise3.rb
@@ -2,5 +2,16 @@
 
 puts "What is your name?"
 name = gets.chomp
+# get input for name
 
 puts "Hi #{name}!"
+
+puts "So #{name}, how old are you?"
+age = gets.chomp
+# get input for age
+
+year_born = 2015 - age.to_i
+# current year minus age 
+
+puts "#{year_born} was an really special year because that's when you were born!"
+
