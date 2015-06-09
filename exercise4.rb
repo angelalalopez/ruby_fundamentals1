0c4ff42d37a67272a7a417b25bc990d367a74bc6
AngeLopez <angel@angelopez.ca>
2015-06-09 14:37:03 -0400

    FizzBuzz problem complete

---
+++ exercise4.rb
@@ -0,0 +1,13 @@
+# FizzBuzz problem
+
+(1..100).each do |num|
+	if (num % 5 == 0 && num % 3 == 0)
+		puts "BitMaker"
+	elsif num % 3 == 0
+		puts "Bit"
+	elsif num % 5 == 0
+		puts "Maker"
+	else 
+		puts num
+	end
+end
