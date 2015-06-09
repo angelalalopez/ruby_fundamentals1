60d8891a1df0ade6d2147a69dea96d85749a1799
AngeLopez <angel@angelopez.ca>
2015-06-09 15:28:12 -0400

    Changes to add month accuracy added

--- exercise3.rb
+++ exercise3.rb
@@ -1,17 +1,30 @@
 # Exercise 3 of assignment 1
 
-puts "What is your name?"
+time_now = Time.new
+month_now = time_now.month
+
+puts "Hello friend, what is your name?"
 name = gets.chomp
-# get input for name
 
-puts "Hi #{name}!"
-
-puts "So #{name}, how old are you?"
-age = gets.chomp
+puts "So, how old are you, #{name}?"
+age = gets.chomp.to_i
 # get input for age
 
-year_born = 2015 - age.to_i
-# current year minus age 
+puts "What month number were you born in? (1-12)"
+month_born = gets.chomp.to_i
+# get input for month number
 
-puts "#{year_born} was an really special year because that's when you were born!"
+month_math = month_now - month_born
+# get positive or negative number
 
+year_now = time_now.year.to_i
+year_born = year_now - age
+
+if month_math > 0
+	year_born
+else month_math < 0
+	year_born -= 1
+end
+# positive is past, negative is yet to happen
+
+puts "#{name}, you were born in #{year_born}!"
